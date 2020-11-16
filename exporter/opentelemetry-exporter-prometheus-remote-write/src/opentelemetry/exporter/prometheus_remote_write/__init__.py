4f837805773414ba1bda436dc3c523676b758614
Shovnik Bhattacharya <shovnik@amazon.com>
2020-11-16 14:57:37 -0500

    Added config implementation, validation and tests

--- exporter/opentelemetry-exporter-prometheus-remote-write/src/opentelemetry/exporter/prometheus_remote_write/__init__.py
+++ exporter/opentelemetry-exporter-prometheus-remote-write/src/opentelemetry/exporter/prometheus_remote_write/__init__.py
@@ -15,31 +15,35 @@
 
 from typing import Dict, Sequence
 
+import yaml
 from opentelemetry.sdk.metrics.export import (
     MetricRecord,
     MetricsExporter,
     MetricsExportResult,
 )
+from opentelemetry.sdk.metrics.export.aggregate import (
+    HistogramAggregator,
+    LastValueAggregator,
+    MinMaxSumCountAggregator,
+    SumAggregator,
+    ValueObserverAggregator,
+)
 
 
-class TimeSeries:
-    """
-    TimeSeries class used to store timeseries labels and samples that need to be sent
-    Args:
-        labels: timeseries labels
-        samples: timeseries samples
-    """
+class TimeSeriesData:
+    def __init__(self, labels, samples):
+        self.labels = labels
+        self.samples = samples
 
-    def __init__(self):
-        pass
+    def __eq__(self, other) -> bool:
+        return self.labels == other.labels and self.samples == other.samples
 
 
 class Config:
     """
     Configuration containing all necessary information to make remote write requests
-
     Args:
-        endpoint: url where data will be sent
+        endpoint: url where data will be sent (Required)
         basic_auth: username and password for authentication (Optional)
         bearer_token: token used for authentication (Optional)
         bearer_token_file: filepath to file containing authentication token (Optional)
@@ -54,10 +58,85 @@ class Config:
         bearer_token_file: str = None,
         headers: Dict = None,
     ):
-        pass
+        self.endpoint = endpoint
+        if basic_auth:
+            self.basic_auth = basic_auth
+        if bearer_token:
+            self.bearer_token = bearer_token
+        if bearer_token_file:
+            self.bearer_token_file = bearer_token_file
+        if headers:
+            self.headers = headers
 
-    def validate(self):
-        pass
+    @property
+    def endpoint(self):
+        return self._endpoint
+
+    @endpoint.setter
+    def endpoint(self, endpoint: str):
+        if endpoint == "":
+            raise ValueError("endpoint required in config")
+        self._endpoint = endpoint
+
+    @property
+    def basic_auth(self):
+        return self._basic_auth
+
+    @basic_auth.setter
+    def basic_auth(self, basic_auth: Dict):
+        if hasattr(self, "bearer_token") or hasattr(self, "bearer_token_file"):
+            raise ValueError(
+                "config cannot contain basic_auth and bearer_token"
+            )
+        if "username" not in basic_auth:
+            raise ValueError("username required in basic_auth")
+        if "password" not in basic_auth and "password_file" not in basic_auth:
+            raise ValueError("password required in basic_auth")
+        if "password" in basic_auth and "password_file" in basic_auth:
+            raise ValueError(
+                "basic_auth cannot contain password and password_file"
+            )
+        self._basic_auth = basic_auth
+
+    @property
+    def bearer_token(self):
+        return self._bearer_token
+
+    @bearer_token.setter
+    def bearer_token(self, bearer_token: str):
+        if hasattr(self, "basic_auth"):
+            raise ValueError(
+                "config cannot contain basic_auth and bearer_token"
+            )
+        if hasattr(self, "bearer_token_file"):
+            raise ValueError(
+                "config cannot contain bearer_token and bearer_token_file"
+            )
+        self._bearer_token = bearer_token
+
+    @property
+    def bearer_token_file(self):
+        return self._bearer_token_file
+
+    @bearer_token_file.setter
+    def bearer_token_file(self, bearer_token_file: str):
+        if hasattr(self, "basic_auth"):
+            raise ValueError(
+                "config cannot contain basic_auth and bearer_token"
+            )
+        if hasattr(self, "bearer_token"):
+            raise ValueError(
+                "config cannot contain bearer_token and bearer_token_file"
+            )
+        self._bearer_token_file = bearer_token_file
+
+    @property
+    def headers(self):
+        return self._headers
+
+    @headers.setter
+    def headers(self, headers: Dict):
+        self._headers = headers
 
 
 class PrometheusRemoteWriteMetricsExporter(MetricsExporter):
@@ -65,7 +144,7 @@ class PrometheusRemoteWriteMetricsExporter(MetricsExporter):
     Prometheus remote write metric exporter for OpenTelemetry.
 
     Args:
-        config: configuration containing all necessary information to make remote write requests
+        config: configuration object containing all necessary information to make remote write requests
     """
 
     def __init__(self, config: Config):
@@ -81,52 +160,63 @@ class PrometheusRemoteWriteMetricsExporter(MetricsExporter):
 
     def convert_to_timeseries(
         self, metric_records: Sequence[MetricRecord]
-    ) -> Sequence[TimeSeries]:
+    ) -> Sequence[TimeSeriesData]:
         pass
 
-    def convert_from_sum(self, sum_record: MetricRecord) -> TimeSeries:
+    def convert_from_sum(self, sum_record: MetricRecord) -> TimeSeriesData:
         pass
 
     def convert_from_min_max_sum_count(
         self, min_max_sum_count_record: MetricRecord
-    ) -> TimeSeries:
+    ) -> TimeSeriesData:
         pass
 
     def convert_from_histogram(
         self, histogram_record: MetricRecord
-    ) -> TimeSeries:
+    ) -> TimeSeriesData:
         pass
 
     def convert_from_last_value(
         self, last_value_record: MetricRecord
-    ) -> TimeSeries:
+    ) -> TimeSeriesData:
         pass
 
     def convert_from_value_observer(
         self, value_observer_record: MetricRecord
-    ) -> TimeSeries:
+    ) -> TimeSeriesData:
         pass
 
-    def convert_from_summary(self, summary_record: MetricRecord) -> TimeSeries:
+    def convert_from_summary(
+        self, summary_record: MetricRecord
+    ) -> TimeSeriesData:
         pass
 
     def sanitize_label(self, label: str) -> str:
         pass
 
-    def build_message(self, data: Sequence[TimeSeries]) -> str:
+    def build_message(self, data: Sequence[TimeSeriesData]) -> str:
         pass
 
     def get_headers(self) -> Dict:
         pass
 
-    def send_message(self, message: str, headers: Dict) -> int:
+    def send_message(self, message: str) -> int:
         pass
 
 
-def parse_config() -> Config:
-    """
-    Method that parses yaml file to generate a valid remote write exporter config
+def parse_config(filepath: str) -> Config:
+    if not filepath.endswith(".yml"):
+        raise ValueError("filepath must point to a .yml file")
+    yaml_dict = {}
+    with open(filepath, "r") as file:
+        yaml_dict = yaml.load(file, Loader=yaml.FullLoader)
 
-    Args:
-        path: filepath to yaml configuration file
-    """
+    endpoint = yaml_dict[0].get("endpoint", [""])[0]
+    basic_auth = yaml_dict[0].get("basic_auth", [{}])[0]
+    bearer_token = yaml_dict[0].get("bearer_token", [""])[0]
+    bearer_token_file = yaml_dict[0].get("bearer_token_file", [""])[0]
+    headers = yaml_dict[0].get("headers", [{}])[0]
+
+    return Config(
+        endpoint, basic_auth, bearer_token, bearer_token_file, headers
+    )
